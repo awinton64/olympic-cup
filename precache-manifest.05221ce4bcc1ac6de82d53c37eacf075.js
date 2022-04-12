7dfc9f0c383b16e7f224c0df487e31ceb7748b11
Andrew Winton <awinton21@gmail.com>
2022-04-12 13:31:27 -0400

    Updates

---
+++ precache-manifest.05221ce4bcc1ac6de82d53c37eacf075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7afdb76545372c6ce0cf",
+    "url": "/olympic-cup/static/css/main.a7b1948f.chunk.css"
+  },
+  {
+    "revision": "7afdb76545372c6ce0cf",
+    "url": "/olympic-cup/static/js/main.7afdb765.chunk.js"
+  },
+  {
+    "revision": "521dee7d6a1a5478516b",
+    "url": "/olympic-cup/static/js/1.521dee7d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/olympic-cup/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "7243c53862c909cd3dd4",
+    "url": "/olympic-cup/static/js/runtime~main.7243c538.js"
+  },
+  {
+    "revision": "eb926f05d358ff71f24baab2f92c50b1",
+    "url": "/olympic-cup/index.html"
+  }
+];
